f5925d9527507410969fbf870b7565414082d590
xlucpu <xlu.cpu@foxmail.com>
2021-04-10 16:27:58 +0800

    latest files

--- R/dataset.R
+++ R/dataset.R
@@ -3,7 +3,7 @@
 #' A data frame storing the subtype-specific markers for MIBC.
 #'
 #' @format A data frame with 120 rows (30 markers for each subtype) and 3 variables.
-#' \describe{contains "templates" the subtype-specific up-regulated markers for MIBC.
+#' \describe{contains ‘templates’ the subtype-specific up-regulated markers for MIBC.
 "templates"
 
 #' Random forest classifier for refining basal-like MIBC
@@ -11,6 +11,6 @@
 #' An R object derived from varSelRF.
 #'
 #' @format An object of class varSelRF
-#' \describe{contains "rfClassifier" an object of class varSelRF and can be used for external prediction (see \link[varSelRF]{varSelRF})
+#' \describe{contains ‘rfClassifier’ an object of class varSelRF and can be used for external prediction.
 "rfClassifier"
 
